bdf713e2e309c37610acfc9175807ad15dc50c70
Victor Bravo <vbravo@fi.uba.ar>
2024-02-11 01:24:48 -0300

    Agrego navbar

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import NavBar from "./components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <AppRouterCacheProvider>
-          {children}
+          <NavBar>
+            {children}
+          </NavBar>
         </AppRouterCacheProvider>
       </body>
     </html>
