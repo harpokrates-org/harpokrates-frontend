83b728fc2e00b9c042c889fc4c9e354f18636383
Victor Bravo <vbravo@fi.uba.ar>
2024-02-12 01:00:39 +0000

    feat: Agrego landing page

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -12,17 +12,8 @@ export default function NavBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Harpokrates
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
