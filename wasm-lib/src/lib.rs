fa9262670227f5052fd50e4df59079b124f59869
EugeniaFranchi <mfranchi@fi.uba.ar>
2024-02-25 18:45:39 +0000

    refactor: better error logs

--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -4,6 +4,7 @@ use wasm_bindgen::prelude::*;
 use petgraph::graph::{DiGraph, NodeIndex};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use utils::set_panic_hook;
 
 #[wasm_bindgen]
 extern "C" {
@@ -38,29 +39,31 @@ pub struct SocialNetwork {
 #[wasm_bindgen]
 impl SocialNetwork {
     pub fn new() -> Self {
+        set_panic_hook();
         Self {
             graph: DiGraph::<String, u32>::new()
         }
     }
 
     pub fn set_net(&mut self, net_json: &str) {
-
-        let net: Net = serde_json::from_str(net_json).expect("Failed to parse input net");
+        set_panic_hook();
+        let net: Net = serde_json::from_str(net_json).expect("SET_NET: Failed to parse input net");
         let mut indexes: HashMap<String, NodeIndex> = HashMap::new();
         
         for node in net.nodes.iter() {
             let index: NodeIndex = self.graph.add_node(node.clone());
             indexes.insert(node.clone(), index);
         }
-
+        
         for edge in net.edges.iter() {
-            let from: &NodeIndex = indexes.get(&edge.0).expect("Node not foud");
-            let to: &NodeIndex = indexes.get(&edge.1).expect("Node not foud");
+            let from: &NodeIndex = indexes.get(&edge.0).expect("SET_NET: Node not foud");
+            let to: &NodeIndex = indexes.get(&edge.1).expect("SET_NET: Node not foud");
             self.graph.add_edge(*from, *to, 1);
         }
     }
-
+    
     pub fn get_net(&mut self) -> String {
+        set_panic_hook();
         let mut net: Net = Net::new();
         let graph_clone_nodes: DiGraph<String, u32> = self.graph.clone();
         let graph_clone_edges: DiGraph<String, u32> = self.graph.clone();
@@ -73,10 +76,10 @@ impl SocialNetwork {
                 let from = edge.source();
                 let to = edge.target();
                 net.edges.push((
-                    (*self.graph.node_weight(from).unwrap()).clone(),
-                    (*self.graph.node_weight(to).unwrap()).clone()
+                    (*self.graph.node_weight(from).expect("GET_NET: Source node not found")).clone(),
+                    (*self.graph.node_weight(to).expect("GET_NET: Target node not found")).clone()
                 ))
             });
-        serde_json::to_string(&net).unwrap()
+        serde_json::to_string(&net).expect("GET_NET: Failed converting net to string")
     }
 }
