78c5717d3d9ee894f12a0d848d108791783eb9f0
Victor Bravo <vbravo@fi.uba.ar>
2024-03-01 06:04:23 -0300

    Reorganise layout

--- app/(console)/photos/page.js
+++ app/(console)/photos/page.js
@@ -1,25 +1,9 @@
 'use client'
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import ImageGallery from './components/ImageGallery';
 import UserSearcher from './components/UserSearcher';
-import SideBar from './components/SideBar';
 import { Grid } from '@mui/material';
-
-const drawerWidth = 240;
 
 export default function ClippedDrawer() {
   return (
