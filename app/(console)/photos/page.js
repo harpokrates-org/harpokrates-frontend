ec2d5d05f1096158a9da137559f6af2e633d005d
EugeniaFranchi <mfranchi@fi.uba.ar>
2024-04-01 11:21:40 -0300

    feat: 🎸 move user searcher to navbar and change emptynet messag
    ✅ Closes: https://trello.com/b/o2JCPlUL/harpokrates

--- app/(console)/photos/page.js
+++ app/(console)/photos/page.js
@@ -2,16 +2,12 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import ImageGallery from './components/ImageGallery';
-import UserSearcher from './components/UserSearcher';
 import { Grid } from '@mui/material';
 
 export default function ClippedDrawer() {
   return (
     <Box>
       <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <UserSearcher />
-        </Grid>
         <Grid item xs={12}>
           <ImageGallery />
         </Grid>
