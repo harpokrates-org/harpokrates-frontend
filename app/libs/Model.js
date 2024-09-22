f5460b7e078365cd8224c65c68749d549c835b70
Victor Bravo <vbravo@fi.uba.ar>
2024-09-22 20:09:36 -0300

    style: 💄 format

--- app/libs/Model.js
+++ app/libs/Model.js
@@ -7,25 +7,25 @@ tf.env().set("PROD", true);
 
 class Model {
   constructor(imgSize, path, threshold) {
-    this.model = null
-    this.imgSize = imgSize
-    this.path = path
-    this.threshold = threshold
+    this.model = null;
+    this.imgSize = imgSize;
+    this.path = path;
+    this.threshold = threshold;
   }
 
   async load() {
-    if (this.model) return this.model
+    if (this.model) return this.model;
     await tf.ready();
     this.model = await tf.loadGraphModel(this.path);
-    return this.model
-  };
+    return this.model;
+  }
 
   async getPrediction(src) {
     const pix = await pixels(src);
     const tensor = this._tensor(pix);
     const prediction = await this._classify(tensor);
     return prediction;
-  };
+  }
 
   _tensor(pix) {
     // Uint8Array -> Tensor 3D RGBA [x,y,4]
@@ -33,7 +33,10 @@ class Model {
     // Tensor 3D RGBA [x,y,4] -> Tensor 3D RGB [x,y,3]
     const rgbTens3d = tf.slice3d(rgbaTens3d, [0, 0, 0], [-1, -1, 3]);
     // Tensor 3D RGB [x,y,3] -> Tensor 3D RGB [100,100,3]
-    const smallImg = tf.image.resizeBilinear(rgbTens3d, [this.imgSize, this.imgSize]);
+    const smallImg = tf.image.resizeBilinear(rgbTens3d, [
+      this.imgSize,
+      this.imgSize,
+    ]);
     // Tensor 3D RGB [100,100,3] -> Tensor 4D RGB [1,100,100,3]
     return smallImg.reshape([1, this.imgSize, this.imgSize, 3]);
   }
@@ -41,7 +44,7 @@ class Model {
   async _classify(tensor) {
     const prediction = this.model.predict(tensor).dataSync()[0];
     return prediction;
-  };
+  }
 }
 
-module.exports = Model
+module.exports = Model;
