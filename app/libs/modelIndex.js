0be3a63d37f2c56301beaeb38727bbcfb43d0848
Victor Bravo <vbravo@fi.uba.ar>
2024-09-26 12:34:28 -0300

    refactor: 💡 model selection on photos

--- app/libs/modelIndex.js
+++ app/libs/modelIndex.js
@@ -4,13 +4,12 @@ const Model = require("./Model");
 const MAIN_PATH = 'model'
 
 const modelNames = {
-  NO_MODEL: 'no_model',
-  LOW_MODEL: 'low_model',
-  EFFICIENTNETV2B0_MODEL: 'efficientnetv2b0',
-  MOBILENETV3L_MODEL: 'mobilenetv3l',
-  INCEPTIONV3_MODEL: 'inceptionv3',
-  VGG16_MODEL: 'vgg16',
-  RESNET_MODEL: 'resnet',
+  NO_MODEL: 'Sin Modelo',
+  EFFICIENTNETV2B0_MODEL: 'EfficientNet',
+  MOBILENETV3L_MODEL: 'MobileNet',
+  INCEPTIONV3_MODEL: 'InceptionNet',
+  VGG16_MODEL: 'VGG16',
+  RESNET_MODEL: 'ResNet',
 }
 
 const modelsData = {
@@ -20,13 +19,7 @@ const modelsData = {
     path: null,
     threshold: 1
   },
-  LOW_MODEL: {
-    name: modelNames.LOW_MODEL,
-    imgSize: 128,
-    path: MAIN_PATH + '/low/model.json',
-    threshold: 0.7
-  },
-  EEFFICIENTNETV2B0_MODEL: {
+  EFFICIENTNETV2B0_MODEL: {
     name: modelNames.EFFICIENTNETV2B0_MODEL,
     imgSize: 512,
     path: MAIN_PATH + '/efficientnetv2b0/model.json',
@@ -61,8 +54,7 @@ const modelsData = {
 module.exports = {
   models: {
     [modelsData.NO_MODEL.name]: NoModel,
-    [modelsData.LOW_MODEL.name]: new Model(modelsData.LOW_MODEL.imgSize, modelsData.LOW_MODEL.path, modelsData.LOW_MODEL.threshold),
-    [modelsData.EEFFICIENTNETV2B0_MODEL.name]: new Model(modelsData.EEFFICIENTNETV2B0_MODEL.imgSize, modelsData.EEFFICIENTNETV2B0_MODEL.path, modelsData.EEFFICIENTNETV2B0_MODEL.threshold),
+    [modelsData.EFFICIENTNETV2B0_MODEL.name]: new Model(modelsData.EFFICIENTNETV2B0_MODEL.imgSize, modelsData.EFFICIENTNETV2B0_MODEL.path, modelsData.EFFICIENTNETV2B0_MODEL.threshold),
     [modelsData.MOBILENETV3L_MODEL.name]: new Model(modelsData.MOBILENETV3L_MODEL.imgSize, modelsData.MOBILENETV3L_MODEL.path, modelsData.MOBILENETV3L_MODEL.threshold),
     [modelsData.INCEPTIONV3_MODEL.name]: new Model(modelsData.INCEPTIONV3_MODEL.imgSize, modelsData.INCEPTIONV3_MODEL.path, modelsData.INCEPTIONV3_MODEL.threshold),
     [modelsData.VGG16_MODEL.name]: new Model(modelsData.VGG16_MODEL.imgSize, modelsData.VGG16_MODEL.path, modelsData.VGG16_MODEL.threshold),
