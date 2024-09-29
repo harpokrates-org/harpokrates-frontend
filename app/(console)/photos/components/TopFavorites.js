75a64a326373da3e3c6958d8f7fb59cb3f804edb
EugeniaFranchi <mfranchi@fi.uba.ar>
2024-09-29 15:28:56 -0300

    feat: 🎸 show top stego favorites

---
+++ app/(console)/photos/components/TopFavorites.js
@@ -0,0 +1,46 @@
+"use client";
+
+import { selectFavorites } from "@/store/FlickrUserSlice";
+import { Box, List, ListItem, ListItemText, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+
+
+export default function TopFavorites() {
+  const favorites = useSelector(selectFavorites)
+
+  return (
+    <Box>
+        <Box marginBottom={2}>
+          <Box marginLeft={2}>
+            <Typography variant="h6">
+              Top Favoritos en Imágenes Estegográficas
+            </Typography>
+
+            {
+              Object.keys(favorites).length === 0 && (
+                <Typography variant="subtitle1" color="textSecondary">
+                  Aún no hay favoritos.
+                </Typography>
+              )
+            }
+
+            <List dense={true}>
+            {
+              Object.keys(favorites).sort((favoriteX, favoriteY) => 
+                        favorites[favoriteY] - favorites[favoriteX])
+                    .slice(0, 10)
+                    .map((favorite) => (
+                      <ListItem>
+                      <ListItemText
+                        primary={favorite}
+                        secondary={favorites[favorite] + ' Favoritos'}
+                      />
+                      </ListItem>
+                    ))
+            }
+            </List>
+          </Box>
+        </Box>
+    </Box>
+  );
+}
--- app/(console)/photos/page.js
+++ app/(console)/photos/page.js
@@ -7,13 +7,16 @@ import EmptyState from '../components/EmptyState';
 import ImageGallery from './components/ImageGallery';
 import PhotosTopBar, { barHeight, margin, titleHeight } from './components/PhotosTopBar';
 import UserProfile from './components/UserProfile';
+import TopFavorites from './components/TopFavorites';
+import { selectFilters } from '@/store/PhotosFilterSlice';
 
 const noPhotosTitle = 'Busca un usuario'
 const noPhotosMessage = 'Para analizar las imágines subidas por un usuario, necesitas ingresar su nombre en el buscador superior.'
 
 export default function ClippedDrawer() {
-  const username = useSelector(selectName)
   const topMargin = barHeight - titleHeight - margin
+  const username = useSelector(selectName)
+  const filters = useSelector(selectFilters);
 
   return (
     <Box>
@@ -22,10 +25,15 @@ export default function ClippedDrawer() {
           <Typography variant="h4">Fotos</Typography>
           <PhotosTopBar />
           { username ? 
-            <>
-              <UserProfile />
-              <ImageGallery />
-            </> :
+              <Box sx={{ display: 'flex' }}>
+                <Box sx={{ width: 3/4 }}>
+                  <UserProfile />
+                  <ImageGallery />
+                </Box>
+                { filters.modelName != 'Sin Modelo' &&
+                  <TopFavorites />
+                }
+              </Box> :
             <EmptyState title={noPhotosTitle} message={noPhotosMessage} topMargin={topMargin} />
           }
         </Grid>
