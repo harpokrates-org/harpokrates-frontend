bb5843203796a7a37159e5bb3c6538db5c612eff
EugeniaFranchi <mfranchi@fi.uba.ar>
2024-11-09 12:31:00 -0300

    fix: 🐛 favorites with different keys

--- app/(console)/photos/components/TopFavorites.js
+++ app/(console)/photos/components/TopFavorites.js
@@ -30,7 +30,7 @@ export default function TopFavorites() {
                         favorites[favoriteY] - favorites[favoriteX])
                     .slice(0, 10)
                     .map((favorite) => (
-                      <ListItem key={favorites}>
+                      <ListItem key={favorite}>
                       <ListItemText
                         primary={favorite}
                         secondary={favorites[favorite] + ' Favoritos'}
